3cc81dbeabe26ad4e55689620fccb9e26b6c0228
Lechulinh <chulinh16e@gmail.com>
2024-10-22 16:00:07 +0700

    hola

--- src/app/menu/page.js
+++ src/app/menu/page.js
@@ -3,7 +3,7 @@ import data from "../../assets/data.json";
 export default function Home() {
   return (
     <main className="menuScreen">
-      <a className="homeBtn" href="/"> quay lại</a>
+      <a className="homeBtn" href="/"> Quay lại</a>
       <div className="menuLevel">
       <ul>
         {data.map((level) => {
